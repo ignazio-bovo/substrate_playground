4a3acda0f86cdfa65de2237e195d658b9a7a2c52
ignazio-bovo <10487328@polimi.it>
2021-05-27 20:21:51 +0200

    started to rebuild utxo

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,4 +1,9 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+use codec::{Decode, Encode};
+use sp_core::H256;
+
+
+#[cfg(feature = "std")]
+// crypto signature scheme to certify transactions
 
 /// Edit this file to define custom logic or remove it if it is not needed.
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
@@ -13,12 +18,21 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+pub type Value = u128;
+
 //#[cfg(feature = "runtime-benchmarks")]
 //mod benchmarking;
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Config: frame_system::Config {
 	/// Because this pallet emits events, it depends on the runtime's definition of an event.
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+}
+
+//#[cfg_attr(feature="std", derive(Serialize, Deserialize))] 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct Utxo { // UTXO
+    pub value: Value, 
+    pub pubScript: H256,
 }
 
 // The pallet's runtime storage items.
@@ -30,8 +44,12 @@ decl_storage! {
 	trait Store for Module<T: Config> as TemplateModule {
 		// Learn more about declaring storage items:
 		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
+        pub RewardTotal get(fn reward_total): Utxo;
+        pub Something: Option<u32>;
 	}
+    add_extra_genesis {
+        config(genesis_utxos): Vec<Utxo>;
+    }
 }
 
 // Pallets use events to inform users when important changes are made.
@@ -59,47 +77,11 @@ decl_error! {
 // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 decl_module! {
 	pub struct Module<T: Config> for enum Call where origin: T::Origin {
+
 		// Errors must be initialized if they are used by the pallet.
 		type Error = Error<T>;
 
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
-
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			Something::put(something);
-
-			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			// Return a successful DispatchResult
-			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
-		}
 	}
 }
